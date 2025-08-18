0ea3a8c3c6b13c51de2ff39f534026baaa3c1178
Daniel Cheng <danielcheng2200@gmail.com>
2025-08-18 04:08:24 -0700

    Recycle Wave 1.4 OFFICIAL RELEASE

--- fnl_CapstoneProject/src/zyzzgames/RunningPlayer.java
+++ fnl_CapstoneProject/src/zyzzgames/RunningPlayer.java
@@ -9,7 +9,7 @@
  * Please contact danielcheng2200@gmail.com if you need additional information
  * or have any questions.
  * 
- * Current Version: 1.3
+ * Current Version: 1.4
  */
 
 package zyzzgames;
